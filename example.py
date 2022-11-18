c7fb1550276b74367abede9d9821075a2d751e50
Moritz Schäfer <moritz.schaefer@pm.me>
2022-11-18 20:09:14 +0100

    basic metric test

--- example.py
+++ example.py
@@ -9,6 +9,7 @@ import jax.numpy as jnp
 import optax
 import tensorflow as tf
 from ase.io import read
+from clu import metrics
 
 from gmnn_jax.model.lin_reg import LinReg
 from gmnn_jax.utils.convert import convert_atoms_to_arrays, tf_to_jax_dict
@@ -23,7 +24,7 @@ def loss_fn(params, model, inputs, labels):
     prediction = model(x=inputs, params=params)
     # jax.debug.print("{prediction}", prediction=prediction)
     loss = optax.l2_loss(prediction, labels["energy"])
-    return jnp.mean(loss)
+    return jnp.mean(loss), prediction
 
 
 atoms_list = read("raw_data/buoh.traj", index=":")
@@ -33,7 +34,8 @@ inputs, labels = convert_atoms_to_arrays(atoms_list)
 ds = tf.data.Dataset.from_tensor_slices((inputs, labels))
 sample_inputs, _ = list(ds.take(1).as_numpy_iterator())[0]  # No batch dim!
 
-ds = ds.batch(2)
+ds = ds.shuffle(32 * 4).batch(2)
+
 
 rng_key = jax.random.PRNGKey(42)
 forward_linreg = hk.without_apply_rng(hk.transform(_forward_linreg))
@@ -47,21 +49,36 @@ opt_state = optimizer.init(params)
 
 @partial(jax.jit, static_argnames=["model"])
 def step(model, params, opt_state, inputs, labels):
-    loss_grad_fn = jax.value_and_grad(loss_fn, 0)  # grad wrt params
-    loss, grads = loss_grad_fn(params, model, inputs, labels)
+    grad_fn = jax.value_and_grad(loss_fn, 0, has_aux=True)  # grad wrt params
+    (loss, prediction), grads = grad_fn(params, model, inputs, labels)
     updates, opt_state = optimizer.update(grads, opt_state, params)
     params = optax.apply_updates(params, updates)
 
-    return params, opt_state, loss
+    return params, opt_state, loss, prediction
 
 
-for epoch in range(1):
+AverageLoss = metrics.Average.from_output("loss")
+
+
+epoch_losses = []
+for epoch in range(3):
+    batch_idx = 0
     for inputs, labels in ds:
         inputs = tf_to_jax_dict(inputs)
         labels = tf_to_jax_dict(labels)
 
-        params, opt_state, loss = step(
+        params, opt_state, loss, prediction = step(
             forward_linreg.apply, params, opt_state, inputs, labels
         )
 
-        print(loss)
+        if batch_idx == 0:
+            batch_loss = AverageLoss.from_model_output(loss=loss)
+        else:
+            new_batch_loss = AverageLoss.from_model_output(loss=loss)
+            batch_loss = batch_loss.merge(new_batch_loss)
+
+        batch_idx += 1
+    epoch_loss = batch_loss.compute()
+    print(epoch_loss)
+
+    epoch_losses.append(epoch_loss)
