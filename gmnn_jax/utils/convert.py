ff33721738bc673755086b8f318c7b16a3de3378
Moritz Schäfer <moritz.schaefer@pm.me>
2022-11-18 16:34:39 +0100

    included forces in convert

--- gmnn_jax/utils/convert.py
+++ gmnn_jax/utils/convert.py
@@ -8,17 +8,17 @@ def convert_atoms_to_arrays(atoms_list: List[Atoms]):
     positions = []
     numbers = []
     energy = []
+    forces = []
 
     for atoms in atoms_list:
         positions.append(atoms.get_positions())
         numbers.append(atoms.get_atomic_numbers())
         energy.append(atoms.get_total_energy())
+        forces.append(atoms.get_forces())
 
     inputs = {
         "positions": np.stack(positions),
         "numbers": np.stack(numbers),
     }
-    labels = {
-        "energy": np.vstack(energy),
-    }
+    labels = {"energy": np.vstack(energy), "forces": np.stack(forces)}
     return inputs, labels
