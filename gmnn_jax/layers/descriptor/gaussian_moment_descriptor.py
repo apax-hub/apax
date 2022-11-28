66c6c0c17e56257cd8f4ee1c6bddb036332e493c
Moritz Schäfer <moritz.schaefer@pm.me>
2022-11-28 21:55:17 +0100

    removed MomentContraction layer

--- gmnn_jax/layers/descriptor/gaussian_moment_descriptor.py
+++ gmnn_jax/layers/descriptor/gaussian_moment_descriptor.py
@@ -40,36 +40,6 @@ class GaussianMomentDescriptor(hk.Module):
             space.canonicalize_displacement_or_metric(displacement)
         )
 
-        # moment_indices = [
-        #     [0],
-        #     [1, 1],
-        #     [2, 2],
-        #     [3, 3],
-        #     [2, 2, 2],
-        #     [1, 1, 2],
-        #     [3, 3, 2],
-        #     [3, 2, 1],
-        # ]
-        # # convention: a corresponds to
-        # # n_atoms, m to n_models, r/s/t to n_radial, i/j/k/l to 3
-        # contr_indices = [
-        #     "ar -> ra",  # Not used in an einsum call, just listed for clarity
-        #     "ari, asi -> rsa",
-        #     "arij, asij -> rsa",
-        #     "arijk, asijk -> rsa",
-        #     "arij, asik, atjk -> rsta",
-        #     "ari, asj, atij -> rsta",
-        #     "arijk, asijl, atkl -> rsta",
-        #     "arijk, asij, atk -> rsta",
-        # ]
-
-        # if use_all_features:
-        #     # triang indices for last contraction are ignored
-        #     # since it does not have any symmetries
-        #     triang_dims = (0, 2, 2, 2, 3, 2, 2, 0)
-        # else:
-        #     triang_dims = (0, 2, 2, 2, 3, 3, 3, 3)
-
         self.triang_idxs_2d = tril_2d_indices(n_radial)
         self.triang_idxs_3d = tril_3d_indices(n_radial)
 
@@ -94,10 +64,8 @@ class GaussianMomentDescriptor(hk.Module):
         # shape: neighbors
         dr_clipped = jnp.clip(dr, a_max=self.r_max)
         cos_cutoff = 0.5 * (jnp.cos(np.pi * dr_clipped / self.r_max) + 1.0)
-        # print(cos_cutoff.dtype)
 
         radial_function = self.radial_fn(dr, Z_i, Z_j, cos_cutoff)
-        # print(radial_function.shape)
 
         moments = geometric_moments(radial_function, dn, neighbor.idx[1], self.n_atoms)
 
@@ -154,16 +122,6 @@ class GaussianMomentDescriptor(hk.Module):
             contr_6,
             contr_7,
         ]
-
-        # for ii, mom in enumerate(moments):
-        #     print(f"MOMENT {ii}")
-        #     print(mom[0])
-
-        # gaussian_moments = []
-        # for i in range(8):
-        #     # gm = self.moment_contractions[i](moments)
-        #     # gaussian_moments.append(gm)
-        #     print(gaussian_moments[i][0,:5])# errors in 4,5,6,7
 
         # gaussian_moments shape: n_atoms x n_features
         gaussian_moments = jnp.concatenate(gaussian_moments, axis=-1)
