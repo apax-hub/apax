390e386130f4216514f3656680b72a2ddd094ee4
Moritz Schäfer <moritz.schaefer@pm.me>
2022-12-13 17:54:51 +0100

    added md config module

---
+++ gmnn_jax/config/md_config.py
@@ -0,0 +1,24 @@
+import os
+
+import yaml
+from pydantic import BaseModel, Extra, PositiveFloat, PositiveInt
+
+
+class MDConfig(BaseModel, frozen=True, extra=Extra.forbid):
+    ckpt_dir: str
+    seed: int = 1
+
+    T: PositiveFloat
+    dt: PositiveFloat = 0.5
+    n_steps: PositiveInt
+    n_inner: PositiveInt = 4
+    dr_threshold: PositiveFloat = 0.5
+    extra_capacity: PositiveInt = 0
+
+    initial_structure: str
+    sim_dir: str = "."
+    traj_name: str = "md.traj"
+
+    def dump_config(self):
+        with open(os.path.join(self.sim_dir, "md_config.yaml"), "w") as conf:
+            yaml.dump(self.dict(), conf, default_flow_style=False)
