ce5e4dcae318d60fc2f9100e8aef368182865aff
Moritz Schäfer <moritz.schaefer@pm.me>
2022-12-15 11:00:04 +0100

    added integration test for the MD utility

---
+++ tests/integration_tests/md/test_md.py
@@ -0,0 +1,70 @@
+from gmnn_jax.md import run_md
+from gmnn_jax.config import Config, MDConfig
+from ase.io import read, write
+import yaml
+from jax_md import partition, space
+import jax.numpy as jnp
+from flax.training import checkpoints
+import jax
+import numpy as np
+from ase import Atoms
+
+from gmnn_jax.model.gmnn import get_training_model
+
+
+def test_run_md():
+    cell_size = 0.
+    
+    with open("config.yaml", "r") as stream:
+        model_config = yaml.safe_load(stream)    
+    with open("md_config.yaml", "r") as stream:
+        md_config = yaml.safe_load(stream)
+
+    model_config = Config.parse_obj(model_config)
+    md_config = MDConfig.parse_obj(md_config)
+
+    positions = np.array([
+        [1.0,0.0,0.0],
+        [0.0,1.0,0.0],
+        [0.0,0.0,1.0],
+    ])
+    atomic_numbers = np.array([1,1,8])
+    cell = np.diag([cell_size]*3)
+    atoms = Atoms(atomic_numbers, positions, cell=cell)
+    write("atoms.extxyz", atoms)
+
+    n_atoms = 3
+    n_species = int(np.max(atomic_numbers) + 1)
+
+    displacement_fn, _ = space.periodic(cell_size)
+
+    neighbor_fn = partition.neighbor_list(
+        displacement_or_metric=displacement_fn,
+        box=10.,
+        r_cutoff=model_config.model.r_max,
+        format=partition.Sparse,
+    )
+    neighbors = neighbor_fn.allocate(positions)
+
+    model_init, _ = get_training_model(
+        n_atoms=n_atoms,
+        n_species=n_species,
+        displacement_fn=displacement_fn,
+        **model_config.model.dict()
+    )
+    rng_key = jax.random.PRNGKey(model_config.seed)
+    params = model_init(rng_key, positions, atomic_numbers, neighbors.idx)
+    ckpt = {"model": {"params": params}, "epoch": 0}
+    checkpoints.save_checkpoint(
+        ckpt_dir=model_config.data.model_path,
+        target=ckpt,
+        step=0,
+        overwrite=True,
+    )
+
+    run_md("config.yaml", "md_config.yaml")
+
+    traj = read(md_config.sim_dir + "/" + md_config.traj_name, index=":")
+    print([atoms.positions for atoms in traj])
+    n_outer = int(md_config.n_steps // md_config.n_inner)
+    assert len(traj) == n_outer
