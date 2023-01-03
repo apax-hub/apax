8f2b3bd72f005c9400734dc3aedd4d36ede79b2b
Nico Segreto <segreto@theochem.uni-stuttgart.de>
2023-01-03 15:57:01 +0100

    input_pipeline now can get a nested dict with non ASE labels dict = {shape: {name: value}} where shape is rigged of fixed name is the name of the lable and values is a np.array of the labels

--- gmnn_jax/data/input_pipeline.py
+++ gmnn_jax/data/input_pipeline.py
@@ -58,6 +58,7 @@ class InputPipeline:
         n_epoch: int,
         batch_size: int,
         atoms_list: list,
+        external_labels: dict,
         buffer_size: int = 1000,
     ) -> None:
         """Processes inputs/labels and makes them accessible for training.
@@ -82,6 +83,10 @@ class InputPipeline:
         self.buffer_size = buffer_size
 
         inputs, labels = convert_atoms_to_arrays(atoms_list)
+
+        if external_labels is not None:
+            for shape in labels.keys():
+                labels[shape].update(external_labels[shape])
 
         self.n_data = len(inputs["fixed"]["n_atoms"])
 
