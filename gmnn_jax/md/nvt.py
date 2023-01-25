61c94ef3a04203f4798d264c71f911c4ae74a2bc
Moritz Schäfer <moritz.schaefer@pm.me>
2023-01-25 11:55:33 +0100

    always enable fp64 config for MD

--- gmnn_jax/md/nvt.py
+++ gmnn_jax/md/nvt.py
@@ -2,6 +2,12 @@ import logging
 import os
 import time
 from functools import partial
+import warnings
+warnings.filterwarnings(
+    action='ignore',
+    category=FutureWarning,
+    module=r'jax.*scatter'
+)
 
 import jax
 import jax.numpy as jnp
@@ -11,8 +17,11 @@ from ase import Atoms, units
 from ase.calculators.singlepoint import SinglePointCalculator
 from ase.io import read
 from ase.io.trajectory import TrajectoryWriter
-from flax.training import checkpoints
 from jax.config import config as jax_config
+
+jax_config.update("jax_enable_x64", True)
+
+from flax.training import checkpoints
 from jax_md import quantity, simulate, space
 from jax_md.util import Array
 from tqdm import trange
@@ -262,8 +271,6 @@ def run_md(
 
     model_config = Config.parse_obj(model_config)
     md_config = MDConfig.parse_obj(md_config)
-
-    jax_config.update("jax_enable_x64", md_config.enable_fp64)
 
     rng_key = jax.random.PRNGKey(md_config.seed)
     md_init_rng_key, rng_key = jax.random.split(rng_key, 2)
