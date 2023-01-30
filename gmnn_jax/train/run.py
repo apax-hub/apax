4430dcfff91dd62f4b34c45755cdc2ba53e680a0
Nico Segreto <segreto@theochem.uni-stuttgart.de>
2023-01-30 11:50:59 +0100

    idxs are now dumped in model_version_path/data-split

--- gmnn_jax/train/run.py
+++ gmnn_jax/train/run.py
@@ -1,3 +1,4 @@
+import csv
 import dataclasses
 import logging
 import os
@@ -17,7 +18,7 @@ from gmnn_jax.optimizer import get_opt
 from gmnn_jax.train.loss import Loss, LossCollection
 from gmnn_jax.train.metrics import initialize_metrics
 from gmnn_jax.train.trainer import fit
-from gmnn_jax.utils.data import load_data, split_list
+from gmnn_jax.utils.data import load_data, split_atoms, split_label
 from gmnn_jax.utils.random import seed_py_np_tf
 
 log = logging.getLogger(__name__)
@@ -130,9 +131,20 @@ def run(user_config, log_file="train.log", log_level="error"):
     if config.data.data_path is not None:
         log.info(f"Read data file {config.data.data_path}")
         atoms_list, label_dict = load_data(config.data.data_path)
-        train_atoms_list, val_atoms_list, train_label_dict, val_label_dict = split_list(
-            atoms_list, label_dict, config.data.n_train, config.data.n_valid
+        train_atoms_list, val_atoms_list, train_idxs, val_idxs = split_atoms(
+            atoms_list, config.data.n_train, config.data.n_valid
         )
+        train_label_dict, val_label_dict = split_label(label_dict, train_idxs, val_idxs)
+        data_split_path = os.path.join(model_version_path, "data-split")
+        os.makedirs(data_split_path, exist_ok=True)
+
+        with open(os.path.join(data_split_path, "train_idxs.csv"), "w") as file:
+            wr = csv.writer(file)
+            wr.writerow(train_idxs)
+        with open(os.path.join(data_split_path, "val_idxs.csv"), "w") as file:
+            wr = csv.writer(file)
+            wr.writerow(val_idxs)
+
     elif config.data.train_data_path and config.data.val_data_path is not None:
         log.info(f"Read training data file {config.data.train_data_path}")
         log.info(f"Read validation data file {config.data.val_data_path}")
