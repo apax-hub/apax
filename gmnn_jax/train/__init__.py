5098024577a0e764067da8f1208cd3130638f443
Nico Segreto <segreto@theochem.uni-stuttgart.de>
2023-01-31 19:53:51 +0100

    new eval fuction

--- gmnn_jax/train/__init__.py
+++ gmnn_jax/train/__init__.py
@@ -2,4 +2,4 @@ from gmnn_jax.train import checkpoints, loss, metrics
 from gmnn_jax.train.run import run
 from gmnn_jax.train.trainer import fit
 
-__all__ = ["checkpoints", "loss", "metrics", "fit", "run"]
+__all__ = ["checkpoints", "loss", "metrics", "fit", "run", "eval"]
