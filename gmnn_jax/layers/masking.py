0131ded9139b0c87e69ad4dcdd0f23d4fde0dd6f
Moritz Schäfer <moritz.schaefer@pm.me>
2023-02-01 16:08:27 +0100

    fixed masking functions condition and shape

--- gmnn_jax/layers/masking.py
+++ gmnn_jax/layers/masking.py
@@ -2,9 +2,9 @@
 import jax.numpy as jnp
 
 def mask_by_atom(arr, Z):
-    mask = (Z == 0).astype(arr.dtype)
+    mask = (Z == 1).astype(arr.dtype)
     return arr * mask
 
 def mask_by_neighbor(arr, idx):
-    mask = (jnp.sum(idx, axis=0) > 0).astype(arr.dtype)
+    mask = (jnp.sum(idx, axis=0) > 0).astype(arr.dtype)[..., None]
     return arr * mask
