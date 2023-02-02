5aff66dc1a73f0a10d4623616d817cdfd6cbe02a
Moritz Schäfer <moritz.schaefer@pm.me>
2023-02-02 13:36:27 +0100

    fixed shape of the atom mask

--- gmnn_jax/layers/masking.py
+++ gmnn_jax/layers/masking.py
@@ -1,9 +1,11 @@
 import jax.numpy as jnp
-
+import einops
 
 def mask_by_atom(arr, Z):
-    mask = (Z == 1).astype(arr.dtype)
-    return arr * mask
+    mask = (Z != 0).astype(arr.dtype)
+    mask = einops.rearrange(mask, "n_atoms -> n_atoms 1")
+    masked_arr = arr * mask
+    return masked_arr
 
 
 def mask_by_neighbor(arr, idx):
