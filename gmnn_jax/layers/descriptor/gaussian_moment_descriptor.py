b3648ed5194b70a5fcd114ab264f7b2715a78ca4
Moritz Schäfer <moritz.schaefer@pm.me>
2023-02-20 11:36:37 +0100

    implemented Flax version of GM descriptor

--- gmnn_jax/layers/descriptor/gaussian_moment_descriptor.py
+++ gmnn_jax/layers/descriptor/gaussian_moment_descriptor.py
@@ -6,14 +6,16 @@ import jax.numpy as jnp
 import numpy as np
 from jax_md import space
 
-from gmnn_jax.layers.descriptor.basis_functions import RadialFunction
+from gmnn_jax.layers.descriptor.basis_functions import RadialFunction, RadialFunctionFlax
 from gmnn_jax.layers.descriptor.moments import geometric_moments
 from gmnn_jax.layers.descriptor.triangular_indices import (
     tril_2d_indices,
     tril_3d_indices,
 )
 from gmnn_jax.layers.masking import mask_by_neighbor
+import flax.linen as nn
 
+from typing import Callable
 
 class GaussianMomentDescriptor(hk.Module):
     def __init__(
@@ -86,6 +88,117 @@ class GaussianMomentDescriptor(hk.Module):
             radial_function = mask_by_neighbor(radial_function, neighbor.idx)
 
         moments = geometric_moments(radial_function, dn, neighbor.idx[1], self.n_atoms)
+
+        contr_0 = moments[0]
+        contr_1 = jnp.einsum("ari, asi -> rsa", moments[1], moments[1])
+        contr_2 = jnp.einsum("arij, asij -> rsa", moments[2], moments[2])
+        contr_3 = jnp.einsum("arijk, asijk -> rsa", moments[3], moments[3])
+        contr_4 = jnp.einsum(
+            "arij, asik, atjk -> rsta", moments[2], moments[2], moments[2]
+        )
+        contr_5 = jnp.einsum("ari, asj, atij -> rsta", moments[1], moments[1], moments[2])
+        contr_6 = jnp.einsum(
+            "arijk, asijl, atkl -> rsta", moments[3], moments[3], moments[2]
+        )
+        contr_7 = jnp.einsum(
+            "arijk, asij, atk -> rsta", moments[3], moments[2], moments[1]
+        )
+
+        n_symm01_features = self.triang_idxs_2d.shape[0] * self.n_radial
+
+        tril_2_i, tril_2_j = self.triang_idxs_2d[:, 0], self.triang_idxs_2d[:, 1]
+        tril_3_i, tril_3_j, tril_3_k = (
+            self.triang_idxs_3d[:, 0],
+            self.triang_idxs_3d[:, 1],
+            self.triang_idxs_3d[:, 2],
+        )
+
+        contr_1 = contr_1[tril_2_i, tril_2_j]
+        contr_2 = contr_2[tril_2_i, tril_2_j]
+        contr_3 = contr_3[tril_2_i, tril_2_j]
+        contr_4 = contr_4[tril_3_i, tril_3_j, tril_3_k]
+        contr_5 = contr_5[tril_2_i, tril_2_j]
+        contr_6 = contr_6[tril_2_i, tril_2_j]
+
+        contr_5 = np.reshape(contr_5, [n_symm01_features, -1])
+        contr_6 = np.reshape(contr_6, [n_symm01_features, -1])
+        contr_7 = np.reshape(contr_7, [self.n_radial**3, -1])
+
+        contr_1 = jnp.transpose(contr_1)
+        contr_2 = jnp.transpose(contr_2)
+        contr_3 = jnp.transpose(contr_3)
+        contr_4 = jnp.transpose(contr_4)
+        contr_5 = jnp.transpose(contr_5)
+        contr_6 = jnp.transpose(contr_6)
+        contr_7 = jnp.transpose(contr_7)
+
+        gaussian_moments = [
+            contr_0,
+            contr_1,
+            contr_2,
+            contr_3,
+            contr_4,
+            contr_5,
+            contr_6,
+            contr_7,
+        ]
+
+        # gaussian_moments shape: n_atoms x n_features
+        gaussian_moments = jnp.concatenate(gaussian_moments, axis=-1)
+        assert gaussian_moments.dtype == self.dtype
+        return gaussian_moments
+
+
+
+class GaussianMomentDescriptorFlax(nn.Module):
+    displacement_fn: Callable = space.free()[0]
+    radial_fn: nn.Module = RadialFunctionFlax()
+    dtype=jnp.float32
+    apply_mask=True
+
+    def setup(self):
+        self.r_max = self.radial_fn.r_max
+        self.n_radial = self.radial_fn.n_radial # TODO: maybe move to call?
+
+        self.displacement = space.map_bond(self.displacement_fn)
+        self.metric = space.map_bond(
+            space.canonicalize_displacement_or_metric(self.displacement_fn)
+        )
+        self.triang_idxs_2d = tril_2d_indices(self.n_radial)
+        self.triang_idxs_3d = tril_3d_indices(self.n_radial)
+
+    def __call__(self, R, Z, neighbor_idxs):
+        # if R.dtype != self.dtype:
+        R = R.astype(self.dtype)
+        # R shape n_atoms x 3
+        # Z shape n_atoms
+        n_atoms = R.shape[0]
+
+        idx_i, idx_j = neighbor_idxs[0], neighbor_idxs[1]
+
+        # shape: neighbors
+        Z_i, Z_j = Z[idx_i], Z[idx_j]
+
+        # dr_vec shape: neighbors x 3
+        dr_vec = self.displacement(
+            R[idx_j], R[idx_i]
+        )  # reverse conventnion to match TF
+        # dr shape: neighbors
+        dr = self.metric(R[idx_i], R[idx_j])
+
+        dr_repeated = einops.repeat(dr + 1e-5, "neighbors -> neighbors 1")
+        # normalized distance vectors, shape neighbors x 3
+        dn = dr_vec / dr_repeated
+
+        # # shape: neighbors TODO delete commment
+        # dr_clipped = jnp.clip(dr, a_max=self.r_max)
+        # cos_cutoff = 0.5 * (jnp.cos(np.pi * dr_clipped / self.r_max) + 1.0)
+
+        radial_function = self.radial_fn(dr, Z_i, Z_j)
+        if self.apply_mask:
+            radial_function = mask_by_neighbor(radial_function, neighbor_idxs)
+
+        moments = geometric_moments(radial_function, dn, idx_j, n_atoms)
 
         contr_0 = moments[0]
         contr_1 = jnp.einsum("ari, asi -> rsa", moments[1], moments[1])
