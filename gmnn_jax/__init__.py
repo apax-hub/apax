92733b6178a7af8ce8daa80f05ac795443059fd7
Moritz Schaefer <moritz.schaefer@pm.me>
2023-02-21 14:01:54 +0100

    stronger enforement of TF GPU restriction

--- gmnn_jax/__init__.py
+++ gmnn_jax/__init__.py
@@ -1,8 +1,14 @@
 import warnings
 
 import tensorflow as tf
+# Disable all GPUS
+tf.config.set_visible_devices([], 'GPU')
+visible_devices = tf.config.get_visible_devices()
+for device in visible_devices:
+    assert device.device_type != 'GPU'
+# tf.config.experimental.set_visible_devices([], "GPU")
+
 from jax.config import config as jax_config
 
 warnings.filterwarnings(action="ignore", category=FutureWarning, module=r"jax.*scatter")
-tf.config.experimental.set_visible_devices([], "GPU")
 jax_config.update("jax_enable_x64", True)
