70893d979784caa20a446174579c191fb107c47e
Moritz Schäfer <moritz.schaefer@pm.me>
2023-02-28 17:59:30 +0100

    get_opt now freezes parameters with lr=0

--- gmnn_jax/optimizer/get_optimizer.py
+++ gmnn_jax/optimizer/get_optimizer.py
@@ -39,6 +39,15 @@ def get_schedule(
     return lr_schedule
 
 
+def make_optimizer(opt, lr, transition_begin, transition_steps, opt_kwargs):
+    if lr <= 1e-7:
+        optimizer = optax.set_to_zero()
+    else:
+        schedule = get_schedule(lr, transition_begin, transition_steps)
+        optimizer = opt(schedule, **opt_kwargs)
+    return optimizer
+
+
 def get_opt(
     params,
     transition_begin: int,
@@ -53,29 +62,34 @@ def get_opt(
 ) -> optax._src.base.GradientTransformation:
     """
     Builds an optimizer with different learning rates for each parameter group.
-    Every optimizer in `optax` is supported.
+    Several `optax` optimizers are supported.
     """
     log.info("Initializing Optimizer")
     opt = getattr(optax, opt_name)
 
-    emb_schedule = get_schedule(emb_lr, transition_begin, transition_steps)
-    nn_schedule = get_schedule(nn_lr, transition_begin, transition_steps)
-    scale_schedule = get_schedule(scale_lr, transition_begin, transition_steps)
-    shift_schedule = get_schedule(shift_lr, transition_begin, transition_steps)
-
     if use_flax:
+        nn_opt = make_optimizer(opt, nn_lr, transition_begin, transition_steps, opt_kwargs)
+        emb_opt = make_optimizer(opt, emb_lr, transition_begin, transition_steps, opt_kwargs)
+        scale_opt = make_optimizer(opt, scale_lr, transition_begin, transition_steps, opt_kwargs)
+        shift_opt = make_optimizer(opt, shift_lr, transition_begin, transition_steps, opt_kwargs)
+
         partition_optimizers = {
-            "w": opt(nn_schedule, **opt_kwargs),
-            "b": opt(nn_schedule, **opt_kwargs),
-            "atomic_type_embedding": opt(emb_schedule, **opt_kwargs),
-            "scale_per_element": opt(scale_schedule, **opt_kwargs),
-            "shift_per_element": opt(shift_schedule, **opt_kwargs),
+            "w": nn_opt,
+            "b": nn_opt,
+            "atomic_type_embedding": emb_opt,
+            "scale_per_element": scale_opt,
+            "shift_per_element": shift_opt,
         }
 
         label_fn = lambda path, v: path[-1]
         param_partitions = freeze(traverse_util.path_aware_map(label_fn, params))
         tx = optax.multi_transform(partition_optimizers, param_partitions)
+
     else:
+        emb_schedule = get_schedule(emb_lr, transition_begin, transition_steps)
+        nn_schedule = get_schedule(nn_lr, transition_begin, transition_steps)
+        scale_schedule = get_schedule(scale_lr, transition_begin, transition_steps)
+        shift_schedule = get_schedule(shift_lr, transition_begin, transition_steps)
         label_fn = map_nested_fn(lambda k, _: k)
         tx = optax.multi_transform(
             {
@@ -83,7 +97,7 @@ def get_opt(
                 "b": opt(nn_schedule, **opt_kwargs),
                 "atomic_type_embedding": opt(
                     emb_schedule, **opt_kwargs
-                ),  # TODO: wrong schedule
+                ),
                 "scale_per_element": opt(scale_schedule, **opt_kwargs),
                 "shift_per_element": opt(shift_schedule, **opt_kwargs),
             },
