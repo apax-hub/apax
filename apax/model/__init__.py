3566a6d774c4b7050d2d38be3624f746f42fd970
Nico Segreto <segreto@theochem.uni-stuttgart.de>
2023-03-02 19:21:10 +0100

    lining

--- apax/model/__init__.py
+++ apax/model/__init__.py
@@ -1,4 +1,4 @@
-from apax.model.gmnn import GMNN, get_md_model, get_training_model
 from apax.model.builder import ModelBuilder
+from apax.model.gmnn import GMNN, get_md_model, get_training_model
 
 __all__ = ["GMNN", "get_training_model", "get_md_model", "ModelBuilder"]
