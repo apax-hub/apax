0897fa1ca84faaa1a5a247e79dd78214de8ca182
Tretracarbonylnickel <segreto.nico@theochem.uni-stuttgart.de>
2023-03-02 17:00:13 +0100

    git action debug

--- apax/train/__init__.py
+++ apax/train/__init__.py
@@ -1,5 +1,5 @@
-from apax.train import checkpoints, loss, metrics
+from apax.train import checkpoints, loss, metrics, eval
 from apax.train.run import run
 from apax.train.trainer import fit
 
-__all__ = ["checkpoints", "loss", "metrics", "fit", "run"]
+__all__ = ["checkpoints", "loss", "metrics", "fit", "run", "eval"]
