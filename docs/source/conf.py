7c03f732202f2082f2839e2f39d48b1b49f35445
Tretracarbonylnickel <segreto.nico@theochem.uni-stuttgart.de>
2023-03-02 14:55:14 +0100

    linting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,14 +9,13 @@
 import os
 import sys
 
-
 sys.path.insert(0, os.path.abspath("."))
 sys.path.insert(0, os.path.abspath("../../"))
 
-project = 'gmnn-jax'
-copyright = '2023, Moritz Schäfer, Nico Segreto, Johannes Kästner'
-author = 'Moritz Schäfer, Nico Segreto, Johannes Kästner'
-release = '0.1.0'
+project = "gmnn-jax"
+copyright = "2023, Moritz Schäfer, Nico Segreto, Johannes Kästner"
+author = "Moritz Schäfer, Nico Segreto, Johannes Kästner"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -29,13 +28,12 @@ extensions = [
     "sphinx.ext.mathjax",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ['_static']
+html_static_path = ["_static"]
