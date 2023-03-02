92d761862e8e9f87955cf228b1b9bd0020051df0
Moritz Schäfer <moritz.schaefer@pm.me>
2023-03-02 11:10:32 +0100

    removed unnecessary comments from init

--- gmnn_jax/__init__.py
+++ gmnn_jax/__init__.py
@@ -3,15 +3,7 @@ import warnings
 import tensorflow as tf
 from jax.config import config as jax_config
 
-# Disable all GPUS
-
-# tf.config.experimental.set_visible_devices([], "GPU")
-
-
 tf.config.set_visible_devices([], "GPU")
-# visible_devices = tf.config.get_visible_devices()
-# for device in visible_devices:
-#     assert device.device_type != 'GPU'
 
 warnings.filterwarnings(action="ignore", category=FutureWarning, module=r"jax.*scatter")
 jax_config.update("jax_enable_x64", True)
