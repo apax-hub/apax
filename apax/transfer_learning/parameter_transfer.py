0390f85df43c711c35202e339159e6a3295aeee5
Moritz Schäfer <moritz.schaefer@pm.me>
2023-03-05 15:30:49 +0100

    removed debug print statement

--- apax/transfer_learning/parameter_transfer.py
+++ apax/transfer_learning/parameter_transfer.py
@@ -9,7 +9,6 @@ def param_transfer(source_params, target_params, param_black_list):
     flat_source = flatten_dict(source_params)
     flat_target = flatten_dict(target_params)
     for p, v in flat_source.items():
-        print(p)
         if p[-2] not in param_black_list:
             flat_target[p] = v
 
