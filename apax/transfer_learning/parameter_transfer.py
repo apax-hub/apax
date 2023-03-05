128c95e8c59c3a8be7ea0679ec00567738c58ad1
Moritz Schäfer <moritz.schaefer@pm.me>
2023-03-05 15:40:07 +0100

    added logging to parameter transfer

--- apax/transfer_learning/parameter_transfer.py
+++ apax/transfer_learning/parameter_transfer.py
@@ -1,6 +1,9 @@
+import logging
+
 from flax.core.frozen_dict import freeze, unfreeze
 from flax.traverse_util import flatten_dict, unflatten_dict
 
+log = logging.getLogger(__name__)
 
 def param_transfer(source_params, target_params, param_black_list):
     source_params = unfreeze(source_params)
@@ -11,6 +14,7 @@ def param_transfer(source_params, target_params, param_black_list):
     for p, v in flat_source.items():
         if p[-2] not in param_black_list:
             flat_target[p] = v
+            log.info("Transfering parameter: %s", p)
 
     transfered_target = unflatten_dict(flat_target)
     transfered_target = freeze(transfered_target)
