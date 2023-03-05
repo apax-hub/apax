eb95de50b4e1d6c7111d13877e904ea00391455f
Moritz Schäfer <moritz.schaefer@pm.me>
2023-03-05 15:40:21 +0100

    added logging to checkpoint loading

--- apax/train/checkpoints.py
+++ apax/train/checkpoints.py
@@ -1,7 +1,8 @@
 from pathlib import Path
-
+import logging 
 from flax.training import checkpoints, train_state
 
+log = logging.getLogger(__name__)
 
 def load_state(model, params, tx, ckpt_dir):
     start_epoch = 0
@@ -13,9 +14,11 @@ def load_state(model, params, tx, ckpt_dir):
     target = {"model": state, "epoch": 0}
     checkpoints_exist = Path(ckpt_dir).is_dir()
     if checkpoints_exist:
+        log.info("Loading checkpoint")
         raw_restored = checkpoints.restore_checkpoint(ckpt_dir, target=target, step=None)
         state = raw_restored["model"]
         start_epoch = raw_restored["epoch"] + 1
+        log.info("Successfully restored checkpoint from epoch %d", raw_restored["epoch"])
 
     return state, start_epoch
 
