b8bd550484fab7d633e19045c0c37551be343d3a
Moritz Schaefer <moritz.schaefer@pm.me>
2023-03-08 09:51:14 +0100

    moved TF and JAX config to train init

--- apax/train/__init__.py
+++ apax/train/__init__.py
@@ -1,3 +1,14 @@
+import warnings
+
+import tensorflow as tf
+from jax.config import config as jax_config
+
+tf.config.set_visible_devices([], "GPU")
+
+warnings.filterwarnings(action="ignore", category=FutureWarning, module=r"jax.*scatter")
+jax_config.update("jax_enable_x64", True)
+
+
 from apax.train import checkpoints, eval, loss, metrics
 from apax.train.run import run
 from apax.train.trainer import fit
