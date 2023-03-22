4fac6575fbcc16bf83885fa45bda54eccb14404d
Tretracarbonylnickel <segreto.nico@theochem.uni-stuttgart.de>
2023-03-22 11:02:15 +0100

    temporarily bug fix #111 memory allocation

--- apax/__init__.py
+++ apax/__init__.py
@@ -1,3 +1,4 @@
+import os
 import warnings
 
 import tensorflow as tf
@@ -5,6 +6,7 @@ from jax.config import config as jax_config
 
 tf.config.set_visible_devices([], "GPU")
 
+os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
 warnings.filterwarnings(action="ignore", category=FutureWarning, module=r"jax.*scatter")
 jax_config.update("jax_enable_x64", True)
 # jax_config.update("jax_debug_nans", True)
