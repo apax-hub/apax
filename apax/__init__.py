958fdd655e07a62b65a07b985868a47abaca3d81
Tretracarbonylnickel <segreto.nico@theochem.uni-stuttgart.de>
2023-03-22 14:16:39 +0100

    comment

--- apax/__init__.py
+++ apax/__init__.py
@@ -9,4 +9,3 @@ tf.config.set_visible_devices([], "GPU")
 os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
 warnings.filterwarnings(action="ignore", category=FutureWarning, module=r"jax.*scatter")
 jax_config.update("jax_enable_x64", True)
-# jax_config.update("jax_debug_nans", True)
