fixed neighbor masking for differently sized systems.

apax/layers/masking.py
@@ -10,5 +10,8 @@ def mask_by_atom(arr, Z):
 
 
 def mask_by_neighbor(arr, idx):
-    mask = (jnp.sum(idx, axis=0) > 0).astype(arr.dtype)[..., None]
+    # mask = (jnp.sum(idx, axis=0) > 0).astype(arr.dtype)[..., None]
+    mask = ((idx[0] - idx[1]) != 0).astype(arr.dtype)
+    if len(arr.shape) == 2:
+        mask = mask[..., None]
     return arr * mask