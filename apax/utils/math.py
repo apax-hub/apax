24a6dcb4b601bedfdef84891ac85dcdfa14d1f09
Moritz R. Schäfer <schaefer@theochem.uni-stuttgart.de>
2023-06-15 13:45:49 +0200

    made normed dotp paddingsafe

--- apax/utils/math.py
+++ apax/utils/math.py
@@ -16,8 +16,10 @@ def normed_dotp(F_0, F_pred):
     F_0_norm = jnp.linalg.norm(F_0, ord=2, axis=2, keepdims=True)
     F_p_norm = jnp.linalg.norm(F_pred, ord=2, axis=2, keepdims=True)
 
-    F_0_n = F_0 / F_0_norm
-    F_p_n = F_pred / F_p_norm
+    eps = 1e-5
+
+    F_0_n = F_0 / (F_0_norm + eps)
+    F_p_n = F_pred / (F_p_norm + eps)
 
     dotp = jnp.einsum("bai, bai -> ba", F_0_n, F_p_n)
     return dotp
