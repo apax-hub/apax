da4f637680082a0f4c786c3653c8441a5dd862c4
Moritz R. Schäfer <schaefer@theochem.uni-stuttgart.de>
2023-10-19 20:54:09 +0200

    moved displacement calculation to model

--- apax/layers/descriptor/gaussian_moment_descriptor.py
+++ apax/layers/descriptor/gaussian_moment_descriptor.py
@@ -15,80 +15,29 @@ from apax.layers.descriptor.triangular_indices import tril_2d_indices, tril_3d_i
 from apax.layers.masking import mask_by_neighbor
 
 
-def disp_fn(ri, rj, perturbation, box):
-    dR = pairwise_displacement(ri, rj)
-    dR = transform(box, dR)
-
-    if perturbation is not None:
-        dR = dR + raw_transform(perturbation, dR)
-        # https://github.com/mir-group/nequip/blob/c56f48fcc9b4018a84e1ed28f762fadd5bc763f1/nequip/nn/_grad_output.py#L267
-        # https://github.com/sirmarcel/glp/blob/main/glp/calculators/utils.py
-        # other codes do R = R + strain, not dR
-        # can be implemented for efficiency
-
-    return dR
-
-
-def get_disp_fn(displacement):
-    def disp_fn(ri, rj, perturbation, box):
-        return displacement(ri, rj, perturbation, box=box)
-
-    return disp_fn
-
-
 class GaussianMomentDescriptor(nn.Module):
     radial_fn: nn.Module = RadialFunction()
     n_contr: int = 8
     dtype: Any = jnp.float32
     apply_mask: bool = True
-    init_box: np.array = field(default_factory=lambda: np.array([0.0, 0.0, 0.0]))
-    inference_disp_fn: Any = None
 
     def setup(self):
         self.r_max = self.radial_fn.r_max
         self.n_radial = self.radial_fn._n_radial
-
-        if np.all(self.init_box < 1e-6):
-            # displacement function for gas phase training and predicting
-            displacement_fn = space.free()[0]
-            self.displacement = space.map_bond(displacement_fn)
-        elif self.inference_disp_fn is None:
-            # displacement function used for training on periodic systems
-            self.displacement = vmap(disp_fn, (0, 0, None, None), 0)
-        else:
-            mappable_displacement_fn = get_disp_fn(self.inference_disp_fn)
-            self.displacement = vmap(mappable_displacement_fn, (0, 0, None, None), 0)
 
         self.distance = vmap(space.distance, 0, 0)
 
         self.triang_idxs_2d = tril_2d_indices(self.n_radial)
         self.triang_idxs_3d = tril_3d_indices(self.n_radial)
 
-    def __call__(
-        self, R, Z, neighbor_idxs, box, offsets=np.full([3, 3], 0), perturbation=None
-    ):
-        R = R.astype(jnp.float64)
-        # R shape n_atoms x 3
+    def __call__(self, dr_vec, Z, neighbor_idxs):
         # Z shape n_atoms
-        n_atoms = R.shape[0]
+        n_atoms = Z.shape[0]
 
         idx_i, idx_j = neighbor_idxs[0], neighbor_idxs[1]
 
         # shape: neighbors
         Z_i, Z_j = Z[idx_i, ...], Z[idx_j, ...]
-
-        Ri = R[idx_i]
-        Rj = R[idx_j]
-
-        # dr_vec shape: neighbors x 3
-        if np.all(self.init_box < 1e-6):
-            # reverse conventnion to match TF
-            # distance vector for gas phase training and predicting
-            dr_vec = self.displacement(Rj, Ri).astype(self.dtype)
-        else:
-            # distance vector for training on periodic systems
-            dr_vec = self.displacement(Rj, Ri, perturbation, box).astype(self.dtype)
-            dr_vec += offsets.astype(self.dtype)
 
         # dr shape: neighbors
         dr = self.distance(dr_vec).astype(self.dtype)
@@ -105,19 +54,13 @@ class GaussianMomentDescriptor(nn.Module):
         moments = geometric_moments(radial_function, dn, idx_j, n_atoms)
 
         contr_0 = moments[0]
-        contr_1 = jnp.einsum("ari, asi -> rsa", moments[1], moments[1])
-        contr_2 = jnp.einsum("arij, asij -> rsa", moments[2], moments[2])
-        contr_3 = jnp.einsum("arijk, asijk -> rsa", moments[3], moments[3])
-        contr_4 = jnp.einsum(
-            "arij, asik, atjk -> rsta", moments[2], moments[2], moments[2]
-        )
-        contr_5 = jnp.einsum("ari, asj, atij -> rsta", moments[1], moments[1], moments[2])
-        contr_6 = jnp.einsum(
-            "arijk, asijl, atkl -> rsta", moments[3], moments[3], moments[2]
-        )
-        contr_7 = jnp.einsum(
-            "arijk, asij, atk -> rsta", moments[3], moments[2], moments[1]
-        )
+        contr_1 = jnp.einsum("ari, asi -> rsa", moments[1], moments[1])                         # noqa: E501
+        contr_2 = jnp.einsum("arij, asij -> rsa", moments[2], moments[2])                       # noqa: E501
+        contr_3 = jnp.einsum("arijk, asijk -> rsa", moments[3], moments[3])                     # noqa: E501
+        contr_4 = jnp.einsum("arij, asik, atjk -> rsta", moments[2], moments[2], moments[2])    # noqa: E501
+        contr_5 = jnp.einsum("ari, asj, atij -> rsta", moments[1], moments[1], moments[2])      # noqa: E501
+        contr_6 = jnp.einsum("arijk, asijl, atkl -> rsta", moments[3], moments[3], moments[2])  # noqa: E501
+        contr_7 = jnp.einsum("arijk, asij, atk -> rsta", moments[3], moments[2], moments[1])    # noqa: E501
 
         n_symm01_features = self.triang_idxs_2d.shape[0] * self.n_radial
 
--- apax/model/gmnn.py
+++ apax/model/gmnn.py
@@ -1,11 +1,14 @@
 import logging
 from dataclasses import field
-from typing import Callable, Tuple, Union
+from typing import Any, Callable, Optional, Tuple, Union
 
 import flax.linen as nn
 import jax
-from jax_md import partition
+from jax_md import partition, space
 from jax_md.util import Array
+import numpy as np
+import jax.numpy as jnp
+from jax import vmap
 
 from apax.layers.descriptor.gaussian_moment_descriptor import GaussianMomentDescriptor
 from apax.layers.empirical import EmpiricalEnergyTerm
@@ -21,6 +24,32 @@ MDModel = Tuple[partition.NeighborFn, Callable, Callable]
 
 log = logging.getLogger(__name__)
 
+def canonicalize_neighbors(neighbor):
+    if type(neighbor) in [partition.NeighborList, NeighborSpoof]:
+        idx = neighbor.idx
+    else:
+        idx = neighbor
+    return idx
+
+
+def disp_fn(ri, rj, perturbation, box):
+    dR = space.pairwise_displacement(ri, rj)
+    dR = space.transform(box, dR)
+
+    if perturbation is not None:
+        dR = dR + space.raw_transform(perturbation, dR)
+        # https://github.com/mir-group/nequip/blob/c56f48fcc9b4018a84e1ed28f762fadd5bc763f1/nequip/nn/_grad_output.py#L267
+        # https://github.com/sirmarcel/glp/blob/main/glp/calculators/utils.py
+        # other codes do R = R + strain, not dR
+        # can be implemented for efficiency
+    return dR
+
+
+def get_disp_fn(displacement):
+    def disp_fn(ri, rj, perturbation, box):
+        return displacement(ri, rj, perturbation, box=box)
+    return disp_fn
+
 
 class AtomisticModel(nn.Module):
     descriptor: nn.Module = GaussianMomentDescriptor()
@@ -30,31 +59,40 @@ class AtomisticModel(nn.Module):
 
     def __call__(
         self,
-        R: Array,
+        dr_vec: Array,
         Z: Array,
-        neighbor: Union[partition.NeighborList, NeighborSpoof, Array],
-        box,
-        offsets,
-        perturbation=None,
+        idx: Array,
+        box: Array,
+        offsets: Array,
+        perturbation: Optional[Array]=None,
     ) -> Array:
-        if type(neighbor) in [partition.NeighborList, NeighborSpoof]:
-            idx = neighbor.idx
-        else:
-            idx = neighbor
-
-        gm = self.descriptor(R, Z, idx, box, offsets, perturbation)
+        gm = self.descriptor(dr_vec, Z, idx, box, offsets, perturbation)
         h = jax.vmap(self.readout)(gm)
         output = self.scale_shift(h, Z)
 
         if self.mask_atoms:
             output = mask_by_atom(output, Z)
-
         return output
 
 
 class EnergyModel(nn.Module):
     atomistic_model: AtomisticModel = AtomisticModel()
     corrections: list[EmpiricalEnergyTerm] = field(default_factory=lambda: [])
+    init_box: np.array = field(default_factory=lambda: np.array([0.0, 0.0, 0.0]))
+    inference_disp_fn: Any = None
+
+    def setup(self):
+
+        if np.all(self.init_box < 1e-6):
+            # gas phase training and predicting
+            displacement_fn = space.free()[0]
+            self.displacement = space.map_bond(displacement_fn)
+        elif self.inference_disp_fn is None:
+            # for training on periodic systems
+            self.displacement = vmap(disp_fn, (0, 0, None, None), 0)
+        else:
+            mappable_displacement_fn = get_disp_fn(self.inference_disp_fn)
+            self.displacement = vmap(mappable_displacement_fn, (0, 0, None, None), 0)
 
     def __call__(
         self,
@@ -65,26 +103,44 @@ class EnergyModel(nn.Module):
         offsets,
         perturbation=None,
     ):
-        atomic_energies = self.atomistic_model(R, Z, neighbor, box, offsets, perturbation)
+        
+        # Distances
+        idx = canonicalize_neighbors(neighbor)
+        idx_i, idx_j = idx[0], idx[1]
+
+        # R shape n_atoms x 3
+        R = R.astype(jnp.float64)
+        Ri = R[idx_i]
+        Rj = R[idx_j]
+
+        # dr_vec shape: neighbors x 3
+        if np.all(self.init_box < 1e-6):
+            # reverse conventnion to match TF
+            # distance vector for gas phase training and predicting
+            dr_vec = self.displacement(Rj, Ri).astype(self.dtype)
+        else:
+            # distance vector for training on periodic systems
+            dr_vec = self.displacement(Rj, Ri, perturbation, box).astype(self.dtype)
+            dr_vec += offsets.astype(self.dtype)
+
+        # Model Core
+        atomic_energies = self.atomistic_model(dr_vec, Z, neighbor)
         total_energy = fp64_sum(atomic_energies)
 
         # Corrections
         for correction in self.corrections:
-            energy_correction = correction(R, Z, neighbor, box, offsets, perturbation)
+            energy_correction = correction(dr_vec, Z, neighbor)
             total_energy = total_energy + energy_correction
+
+        # TODO think of nice abstraction for predicting additional properties
         return total_energy
 
 
 class EnergyDerivativeModel(nn.Module):
-    atomistic_model: AtomisticModel = AtomisticModel()
+    # Alternatively, should this be a function transformation?
+    energy_model: EnergyModel = EnergyModel()
     corrections: list[EmpiricalEnergyTerm] = field(default_factory=lambda: [])
     calc_stress: bool = False
-
-    def setup(self):
-        self.energy_fn = EnergyModel(
-            atomistic_model=self.atomistic_model,
-            corrections=self.corrections,
-        )
 
     def __call__(
         self,
@@ -94,7 +150,7 @@ class EnergyDerivativeModel(nn.Module):
         box,
         offsets,
     ):
-        energy, neg_forces = jax.value_and_grad(self.energy_fn)(
+        energy, neg_forces = jax.value_and_grad(self.energy_model)(
             R, Z, neighbor, box, offsets
         )
         forces = -neg_forces
@@ -102,7 +158,7 @@ class EnergyDerivativeModel(nn.Module):
 
         if self.calc_stress:
             stress = stress_times_vol(
-                self.energy_fn, R, box, Z=Z, neighbor=neighbor, offsets=offsets
+                self.energy_model, R, box, Z=Z, neighbor=neighbor, offsets=offsets
             )
             prediction["stress"] = stress
 
