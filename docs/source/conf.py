ccb903c4925161ee73cbdb7d6bbac424803940a4
Moritz R. Schäfer <schaefer@theochem.uni-stuttgart.de>
2023-11-13 10:05:19 +0100

    add furo theme to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,17 +35,8 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
-# html_static_path = ["_static"]
-
+html_theme = "furo"
 
 html_theme_options = {
     'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
 }
