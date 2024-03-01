f5a7fd1f7b48d20353ccefbb7c607f2d5f950f38
Moritz R. Schäfer <schaefer@theochem.uni-stuttgart.de>
2024-03-01 14:46:05 +0100

    removed autodoc typehints

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,6 @@ release = "0.1.0"
 
 extensions = [
     "sphinx.ext.autodoc",
-    "sphinx_autodoc_typehints",
     "sphinx.ext.napoleon",
     "sphinx.ext.autosummary",
     "sphinx.ext.mathjax",
