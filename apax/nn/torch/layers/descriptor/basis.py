a1fa92482ad3ebfe8654ba06098891a8d13f89f0
Moritz R. Schäfer <schaefer@theochem.uni-stuttgart.de>
2024-04-02 18:03:11 +0200

    implemented torch descriptor

---
+++ apax/nn/torch/layers/descriptor/basis.py
@@ -0,0 +1,62 @@
+from typing import Any
+
+import einops
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+from apax.nn.impl.basis import cosine_cutoff, gaussian_basis_impl, radial_basis_impl
+
+
+class GaussianBasis(nn.Module):
+    def __init__(self, n_basis: int = 7, r_min: float = 0.5, r_max: float = 6.0, dtype: Any = torch.float32) -> None:
+        super().__init__()
+        self.n_basis = n_basis
+        self.r_min = r_min
+        self.r_max = r_max
+        self.dtype = dtype
+
+        self.betta = self.n_basis**2 / self.r_max**2
+        self.rad_norm = (2.0 * self.betta / np.pi) ** 0.25
+        shifts = self.r_min + (self.r_max - self.r_min) / self.n_basis * np.arange(
+            self.n_basis
+        )
+
+        # shape: 1 x n_basis
+        shifts = einops.repeat(shifts, "n_basis -> 1 n_basis")
+        self.shifts = torch.tensor(shifts, dtype=self.dtype)
+
+    def forward(self, dr: torch.Tensor) -> torch.Tensor:
+        basis = gaussian_basis_impl(dr.type(self.dtype), self.shifts, self.betta, self.rad_norm)
+        return basis
+
+
+class RadialFunction(nn.Module):
+    def __init__(self, n_radial: int = 5, basis_fn: nn.Module = GaussianBasis(), n_species: int = 119, dtype: Any = torch.float32) -> None:
+        super().__init__()
+        self.n_radial = n_radial
+        self.basis_fn = basis_fn
+        self.n_species = n_species
+        self.dtype = dtype
+
+        self.r_max = self.basis_fn.r_max
+        norm = 1.0 / np.sqrt(self.basis_fn.n_basis)
+        self.embed_norm = torch.Tensor(norm , dtype=self.dtype)
+        self.embeddings = None
+        if self.emb_init is not None:
+            self.embeddings = nn.Parameter()
+        else:
+            self._n_radial = self.basis_fn.n_basis
+
+    def forward(self, dr, Z_i, Z_j):
+        dr = dr.type(self.dtype)
+        # basis shape: neighbors x n_basis
+        basis = self.basis_fn(dr)
+
+        radial_function = radial_basis_impl(basis, Z_i, Z_j, self.embeddings, self.embed_norm)
+        cutoff = cosine_cutoff(dr, self.r_max)
+        radial_function = radial_function * cutoff
+
+        assert radial_function.dtype == self.dtype
+        return radial_function
---
+++ apax/nn/torch/layers/descriptor/gaussian_moment_descriptor.py
@@ -0,0 +1,61 @@
+from typing import Any
+
+import einops
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from apax.nn.impl.gaussian_moment_descriptor import gaussian_moment_impl
+from apax.nn.torch.layers.descriptor.basis import RadialFunction
+from apax.nn.impl.moments import geometric_moments
+from apax.nn.impl.triangular_indices import tril_2d_indices, tril_3d_indices
+from apax.utils.jax_md_reduced import space
+
+
+def distance(dR):
+    return torch.sqrt(torch.sum(dR**2, axis=-1))
+
+
+class GaussianMomentDescriptor(nn.Module):
+    
+    def __init__(self, radial_fn: nn.Module = RadialFunction(), n_contr: int = 8, dtype: Any = torch.float32):
+        super().__init__()
+        self.radial_fn = radial_fn
+        self.n_contr = n_contr
+        self.dtype = dtype
+
+        self.r_max = self.radial_fn.r_max
+        self.n_radial = self.radial_fn._n_radial
+
+        self.distance = torch.vmap(distance, 0, 0)
+
+        self.triang_idxs_2d = torch.tensor(tril_2d_indices(self.n_radial))
+        self.triang_idxs_3d = torch.tensor(tril_3d_indices(self.n_radial))
+
+    def forward(self, dr_vec: torch.Tensor, Z: torch.Tensor, neighbor_idxs: torch.Tensor) -> torch.Tensor:
+        dr_vec = dr_vec.type(self.dtype)
+        # Z shape n_atoms
+        n_atoms = Z.shape[0]
+
+        idx_i, idx_j = neighbor_idxs[0], neighbor_idxs[1]
+
+        # shape: neighbors
+        Z_i, Z_j = Z[idx_i, ...], Z[idx_j, ...]
+
+        # dr shape: neighbors
+        dr = self.distance(dr_vec)
+
+        # TODO: maybe try jnp where
+        dr_repeated = einops.repeat(dr + 1e-5, "neighbors -> neighbors 1")
+        # normalized distance vectors, shape neighbors x 3
+        dn = dr_vec / dr_repeated
+
+        radial_function = self.radial_fn(dr, Z_i, Z_j)
+
+        moments = geometric_moments(radial_function, dn, idx_j, n_atoms)
+        gaussian_moments = gaussian_moment_impl(moments, self.triang_idxs_2d, self.triang_idxs_3d, self.n_contr)
+
+        # # gaussian_moments shape: n_atoms x n_features
+        # gaussian_moments = jnp.concatenate(gaussian_moments[: self.n_contr], axis=-1)
+        assert gaussian_moments.dtype == self.dtype
+        return gaussian_moments
