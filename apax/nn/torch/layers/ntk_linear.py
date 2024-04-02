59246c33a52e4462f3b40e7cf809b3342ce24639
Moritz R. Schäfer <schaefer@theochem.uni-stuttgart.de>
2024-04-02 17:22:20 +0200

    implemented NTK linear

---
+++ apax/nn/torch/layers/ntk_linear.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class NTKLinear(nn.Module):
+
+    def __init__(self, units) -> None:
+        super().__init__()
+
+        self.bias_factor = 0.1
+        # self.weight_factor = torch.sqrt(1.0 / dim_in)
+
+        self.w = nn.Parameter()
+        self.b = nn.Parameter()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        weight_factor = torch.sqrt(1.0 / x.shape[0])
+        out = F.linear(x, weight_factor * self.w, self.bias_factor * self.b)
+        return out
