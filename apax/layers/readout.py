8e1b4d0307ef691bbad5cdbc0311e2c29465de41
Moritz R. Schäfer <schaefer@theochem.uni-stuttgart.de>
2024-05-04 10:10:01 +0200

    added possibility for shallow ens in readout

--- apax/layers/readout.py
+++ apax/layers/readout.py
@@ -12,17 +12,22 @@ class AtomisticReadout(nn.Module):
     units: List[int] = field(default_factory=lambda: [512, 512])
     activation_fn: Callable = swish
     b_init: str = "normal"
+    n_shallow_ensemble: int = 0
     dtype: Any = jnp.float32
 
     def setup(self):
-        units = [u for u in self.units] + [1]
+        readout_unit = [1]
+        if self.n_shallow_ensemble > 0:
+            readout_unit = [self.n_shallow_ensemble]
+            self._n_shallow_ensemble = self.n_shallow_ensemble
+
+        units = [u for u in self.units] + readout_unit
         dense = []
         for ii, n_hidden in enumerate(units):
-            dense.append(
-                NTKLinear(
-                    n_hidden, b_init=self.b_init, dtype=self.dtype, name=f"dense_{ii}"
-                )
+            layer = NTKLinear(
+                n_hidden, b_init=self.b_init, dtype=self.dtype, name=f"dense_{ii}"
             )
+            dense.append(layer)
             if ii < len(units) - 1:
                 dense.append(swish)
         self.sequential = nn.Sequential(dense, name="readout")
