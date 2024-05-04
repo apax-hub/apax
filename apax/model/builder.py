705f109751e767ba742f4addf84c0e3a517b0d33
Moritz R. Schäfer <schaefer@theochem.uni-stuttgart.de>
2024-05-04 10:32:01 +0200

    added shallow ens to builder

--- apax/model/builder.py
+++ apax/model/builder.py
@@ -6,7 +6,12 @@ from apax.layers.descriptor.gaussian_moment_descriptor import GaussianMomentDesc
 from apax.layers.empirical import ZBLRepulsion
 from apax.layers.readout import AtomisticReadout
 from apax.layers.scaling import PerElementScaleShift
-from apax.model.gmnn import AtomisticModel, EnergyDerivativeModel, EnergyModel
+from apax.model.gmnn import (
+    AtomisticModel,
+    EnergyDerivativeModel,
+    EnergyModel,
+    ShallowEnsembleModel,
+)
 
 
 class ModelBuilder:
@@ -121,9 +126,14 @@ class ModelBuilder:
             init_box=init_box,
             inference_disp_fn=inference_disp_fn,
         )
-
-        model = EnergyDerivativeModel(
-            energy_model,
-            calc_stress=self.config["calc_stress"],
-        )
+        if self.config.n_shallow_ensemble > 0:
+            model = ShallowEnsembleModel(
+                energy_model,
+                calc_stress=False,  # self.config["calc_stress"],
+            )
+        else:
+            model = EnergyDerivativeModel(
+                energy_model,
+                calc_stress=self.config["calc_stress"],
+            )
         return model
