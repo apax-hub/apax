fixed vectorized readout model. implemented pbc calculation

apax/nn/torch/layers/descriptor/gaussian_moment_descriptor.py
@@ -1,21 +1,12 @@
 from typing import Any
 
-import einops
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import torch_scatter
 
-from apax.nn.torch.layers.descriptor.basis import RadialFunctionT
 from apax.nn.impl.triangular_indices import tril_2d_indices, tril_3d_indices
+from apax.nn.torch.layers.descriptor.basis import RadialFunctionT
+from apax.nn.torch.layers.descriptor.math import segment_sum
 
-
-# def distance(dR):
-#     return torch.sqrt(torch.sum(dR**2, axis=-1))
-
-def segment_sum(x, segment_ids):
-    out = torch_scatter.scatter(x, segment_ids, dim=0, reduce="sum")
-    return out
 
 def geometric_moments(radial_function, dn, idx_i, n_atoms):
     # dn shape: neighbors x 3
@@ -23,22 +14,18 @@ def geometric_moments(radial_function, dn, idx_i, n_atoms):
     xyz = dn[:, None, :]
     xyz2 = xyz[..., None, :]
     xyz3 = xyz2[..., None, :]
-    # print(dn.shape)
-    # print(xyz.shape)
-    # print(xyz3.shape)
-    # quit()
 
     # shape: n_neighbors x n_radial x (3)^(moment_number)
     zero_moment = radial_function
-    first_moment = zero_moment[..., None]  * xyz
+    first_moment = zero_moment[..., None] * xyz
     second_moment = first_moment[..., None] * xyz2
-    third_moment = second_moment[..., None]  * xyz3
+    third_moment = second_moment[..., None] * xyz3
 
     # shape: n_atoms x n_radial x (3)^(moment_number)
-    zero_moment = segment_sum(zero_moment, idx_i)
-    first_moment = segment_sum(first_moment, idx_i)
-    second_moment = segment_sum(second_moment, idx_i)
-    third_moment = segment_sum(third_moment, idx_i)
+    zero_moment = segment_sum(zero_moment, idx_i, dim_size=n_atoms)
+    first_moment = segment_sum(first_moment, idx_i, dim_size=n_atoms)
+    second_moment = segment_sum(second_moment, idx_i, dim_size=n_atoms)
+    third_moment = segment_sum(third_moment, idx_i, dim_size=n_atoms)
 
     moments = [zero_moment, first_moment, second_moment, third_moment]
 
@@ -51,7 +38,7 @@ class GaussianMomentDescriptorT(nn.Module):
         radial_fn: nn.Module = RadialFunctionT(),
         n_contr: int = 8,
         dtype: Any = torch.float32,
-        params = None
+        params=None,
     ):
         super().__init__()
 
@@ -64,8 +51,6 @@ class GaussianMomentDescriptorT(nn.Module):
 
         self.r_max = self.radial_fn.r_max
         self.n_radial: int = self.radial_fn.n_radial
-
-        # self.distance = distance
 
         self.triang_idxs_2d = torch.tensor(tril_2d_indices(self.n_radial))
         self.triang_idxs_3d = torch.tensor(tril_3d_indices(self.n_radial))
@@ -90,16 +75,25 @@ class GaussianMomentDescriptorT(nn.Module):
         dn = dr_vec / dr_repeated
 
         radial_function = self.radial_fn(dr, Z_i, Z_j)
-        moments = geometric_moments(radial_function, dn, idx_j, self.dummy_n_atoms)
+        n_atoms: torch.Tensor = torch.tensor(Z.shape[0])
+        moments = geometric_moments(radial_function, dn, idx_j, n_atoms)
 
         contr_0 = moments[0]
         contr_1 = torch.einsum("ari, asi -> rsa", [moments[1], moments[1]])
         contr_2 = torch.einsum("arij, asij -> rsa", [moments[2], moments[2]])
         contr_3 = torch.einsum("arijk, asijk -> rsa", [moments[3], moments[3]])
-        contr_4 = torch.einsum("arij, asik, atjk -> rsta", [moments[2], moments[2], moments[2]])
-        contr_5 = torch.einsum("ari, asj, atij -> rsta", [moments[1], moments[1], moments[2]])
-        contr_6 = torch.einsum("arijk, asijl, atkl -> rsta", [moments[3], moments[3], moments[2]])
-        contr_7 = torch.einsum("arijk, asij, atk -> rsta", [moments[3], moments[2], moments[1]])
+        contr_4 = torch.einsum(
+            "arij, asik, atjk -> rsta", [moments[2], moments[2], moments[2]]
+        )
+        contr_5 = torch.einsum(
+            "ari, asj, atij -> rsta", [moments[1], moments[1], moments[2]]
+        )
+        contr_6 = torch.einsum(
+            "arijk, asijl, atkl -> rsta", [moments[3], moments[3], moments[2]]
+        )
+        contr_7 = torch.einsum(
+            "arijk, asij, atk -> rsta", [moments[3], moments[2], moments[1]]
+        )
 
         # n_symm01_features = triang_idxs_2d.shape[0] * n_radial
         tril_2_i, tril_2_j = self.triang_idxs_2d[:, 0], self.triang_idxs_2d[:, 1]
@@ -143,6 +137,6 @@ class GaussianMomentDescriptorT(nn.Module):
         ]
 
         # gaussian_moments shape: n_atoms x n_features
-        gaussian_moments = torch.concatenate(gaussian_moments[:self.n_contr], dim=-1)
+        gaussian_moments = torch.concatenate(gaussian_moments[: self.n_contr], dim=-1)
         assert gaussian_moments.dtype == self.dtype
         return gaussian_moments

apax/nn/torch/layers/ntk_linear.py
@@ -1,7 +1,7 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np
 
 
 class NTKLinearT(nn.Module):
@@ -22,6 +22,6 @@ class NTKLinearT(nn.Module):
         self.one = torch.tensor(1.0)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        weight_factor = torch.sqrt(self.one / x.size(0))
+        weight_factor = torch.sqrt(self.one / x.size(1))
         out = F.linear(x, weight_factor * self.w, self.bias_factor * self.b)
         return out

apax/nn/torch/model/gmnn.py
@@ -1,9 +1,8 @@
 from typing import Any, List, Optional
-import einops
+
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch import autograd
 
 from apax.nn.torch.layers.descriptor import GaussianMomentDescriptorT
@@ -17,7 +16,7 @@ class AtomisticModelT(nn.Module):
         descriptor: nn.Module = GaussianMomentDescriptorT(),
         readout: nn.Module = AtomisticReadoutT(),
         scale_shift: nn.Module = PerElementScaleShiftT(),
-        params = None
+        params=None,
     ):
         super().__init__()
 
@@ -25,10 +24,7 @@ class AtomisticModelT(nn.Module):
             self.descriptor = GaussianMomentDescriptorT(params=params["descriptor"])
             readout = AtomisticReadoutT(params_list=params["readout"])
 
-            def readout_fn(x):
-                return readout(x)
-
-            self.readout = readout#torch.vmap(readout_fn)
+            self.readout = readout
             self.scale_shift = PerElementScaleShiftT(params=params["scale_shift"])
         else:
             self.descriptor = descriptor
@@ -42,12 +38,7 @@ class AtomisticModelT(nn.Module):
         idx: torch.Tensor,
     ) -> torch.Tensor:
         gm = self.descriptor(dr_vec, Z, idx)
-        
-        h = []
-        for g in gm:
-            hi = self.readout(g)
-            h.append(hi)
-        h = torch.concat(h, dim=0)
+        h = self.readout(gm).squeeze()
         output = self.scale_shift(h, Z)
         return output
 
@@ -56,18 +47,10 @@ def free_displacement(Ri, Rj):
     return Ri - Rj
 
 
-def get_displacement(init_box, inference_disp_fn):
-    if np.all(init_box < 1e-6):
-        # gas phase training and predicting
-        displacement = free_displacement
-    # elif inference_disp_fn is None:
-    #     # for training on periodic systems
-    #     displacement = vmap(disp_fn, (0, 0, None, None), 0)
-    # else:
-    #     mappable_displacement_fn = get_disp_fn(self.inference_disp_fn)
-    #     displacement = vmap(mappable_displacement_fn, (0, 0, None, None), 0)
-
-    return displacement
+def periodic_displacement(Ri, Rj, box):
+    dr = free_displacement(Ri, Rj)
+    dr = torch.matmul(dr, box)
+    return dr
 
 
 class EnergyModelT(nn.Module):
@@ -75,7 +58,7 @@ class EnergyModelT(nn.Module):
         self,
         atomistic_model: AtomisticModelT = AtomisticModelT(),
         # corrections: list[EmpiricalEnergyTerm] = field(default_factory=lambda: []),
-        params = None,
+        params=None,
         init_box: np.array = np.array([0.0, 0.0, 0.0]),
         inference_disp_fn: Any = None,
     ):
@@ -86,9 +69,6 @@ class EnergyModelT(nn.Module):
             self.atomistic_model = atomistic_model
         # self.corrections = corrections
         self.init_box = torch.tensor(init_box)
-        self.inference_disp_fn = inference_disp_fn
-
-        self.displacement = get_displacement(init_box, inference_disp_fn)
 
     def forward(
         self,
@@ -108,13 +88,12 @@ class EnergyModelT(nn.Module):
         Rj = R[idx_j]
 
         # dr_vec shape: neighbors x 3
-        dr_vec = self.displacement(Rj, Ri)
         # uncomment once pbc are implemented
-        # if torch.all(self.init_box < 1e-6):
-        #     dr_vec = self.displacement(Rj, Ri)
-        # else:
-        #     dr_vec = self.displacement(Rj, Ri, perturbation, box)
-        #     dr_vec += offsets
+        if torch.all(box < 1e-6):
+            dr_vec = free_displacement(Rj, Ri)
+        else:
+            dr_vec = periodic_displacement(Rj, Ri, box)
+            dr_vec += offsets
 
         # Model Core
         atomic_energies = self.atomistic_model(dr_vec, Z, idx)
@@ -155,7 +134,7 @@ class EnergyDerivativeModelT(nn.Module):
         requires_grad = [R]
 
         energy = self.energy_model(R, Z, neighbor, box, offsets)
-        grad_outputs : List[Optional[torch.Tensor]] = [torch.ones_like(energy)]
+        grad_outputs: List[Optional[torch.Tensor]] = [torch.ones_like(energy)]
 
         forces = autograd.grad(
             [energy],
@@ -164,7 +143,7 @@ class EnergyDerivativeModelT(nn.Module):
             create_graph=True,
         )[0]
         assert forces is not None
-        forces = - forces
+        forces = -forces
 
         prediction = {"energy": energy, "forces": forces}
 