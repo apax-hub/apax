d17a8bf0475bc64455830bc7fb9668d4a490ee67
Moritz R. Schäfer <schaefer@theochem.uni-stuttgart.de>
2024-05-22 11:30:26 +0200

    added ApaxImport node

--- apax/nodes/__init__.py
+++ apax/nodes/__init__.py
@@ -1,4 +1,4 @@
 from .md import ApaxJaxMD
-from .model import Apax, ApaxEnsemble
+from .model import Apax, ApaxEnsemble, ApaxImport
 
-__all__ = ["Apax", "ApaxEnsemble", "ApaxJaxMD"]
+__all__ = ["Apax", "ApaxEnsemble", "ApaxJaxMD", "ApaxImport"]
