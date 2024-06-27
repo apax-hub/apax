8f05c483d7e6a93ffdfe064a2c3a2cb4393c5a10
Moritz R. Schäfer <schaefer@theochem.uni-stuttgart.de>
2024-06-27 16:24:04 +0200

    improved data pipeline logging

--- apax/data/initialization.py
+++ apax/data/initialization.py
@@ -24,13 +24,14 @@ def load_data_files(data_config):
 
     log.info("Running Input Pipeline")
     if data_config.data_path is not None:
-        log.info(f"Read data file {data_config.data_path}")
+        log.info(f"Reading data file {data_config.data_path}")
         atoms_list = load_data(data_config.data_path)
 
         train_idxs, val_idxs = split_idxs(
             atoms_list, data_config.n_train, data_config.n_valid
         )
         train_atoms_list, val_atoms_list = split_atoms(atoms_list, train_idxs, val_idxs)
+        log.info(f"Found n_train: {len(train_atoms_list)}, n_val: {len(val_atoms_list)}")
 
         np.savez(
             data_config.model_version_path / "train_val_idxs",
@@ -39,10 +40,11 @@ def load_data_files(data_config):
         )
 
     elif data_config.train_data_path and data_config.val_data_path is not None:
-        log.info(f"Read training data file {data_config.train_data_path}")
-        log.info(f"Read validation data file {data_config.val_data_path}")
+        log.info(f"Reading training data file {data_config.train_data_path}")
+        log.info(f"Reading validation data file {data_config.val_data_path}")
         train_atoms_list = load_data(data_config.train_data_path)
         val_atoms_list = load_data(data_config.val_data_path)
+        log.info(f"Found n_train: {len(train_atoms_list)}, n_val: {len(val_atoms_list)}")
     else:
         raise ValueError("input data path/paths not defined")
 
