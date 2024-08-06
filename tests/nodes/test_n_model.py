8e0c95d77b9b7de4919a257b1caf68ac2b0ad784
Moritz R. Schäfer <schaefer@theochem.uni-stuttgart.de>
2024-08-06 12:35:03 +0200

    moved BAL selection node from ips to apax

--- tests/nodes/test_n_model.py
+++ tests/nodes/test_n_model.py
@@ -1,13 +1,17 @@
 import os
 import pathlib
 
+import ipsuite as ips
+import numpy as np
 import yaml
 import zntrack
 
+import apax.nodes
 from apax.nodes.model import Apax, ApaxEnsemble
 from apax.nodes.utils import AddData
 
 CONFIG_PATH = pathlib.Path(__file__).parent / "example.yaml"
+TEST_PATH = pathlib.Path(__file__).parent.resolve()
 
 
 def save_config_with_seed(path: str, seed: int = 1) -> None:
@@ -46,11 +50,37 @@ def test_n_train_2_model(tmp_path, get_md22_stachyose):
     save_config_with_seed(tmp_path / "example2.yaml", seed=2)
 
     proj = zntrack.Project(automatic_node_names=True)
+    thermostat = ips.calculators.LangevinThermostat(
+        time_step=1.0, temperature=100.0, friction=0.01
+    )
     with proj:
         data = AddData(file=get_md22_stachyose)
         model1 = Apax(data=data.atoms, validation_data=data.atoms, config="example.yaml")
         model2 = Apax(data=data.atoms, validation_data=data.atoms, config="example2.yaml")
         ensemble = ApaxEnsemble(models=[model1, model2])
+        md = ips.calculators.ASEMD(
+            data=data.atoms,
+            model=ensemble,
+            thermostat=thermostat,
+            steps=20,
+            sampling_rate=1,
+        )
+
+        uncertainty_selection = ips.configuration_selection.ThresholdSelection(
+            data=md, n_configurations=1, threshold=0.0001
+        )
+
+        selection_batch_size = 3
+        kernel_selection = apax.nodes.BatchKernelSelection(
+            data=md.atoms,
+            train_data=data.atoms,
+            models=[model1, model2],
+            n_configurations=selection_batch_size,
+            processing_batch_size=4,
+        )
+
+        prediction = ips.analysis.Prediction(data=kernel_selection.atoms, model=ensemble)
+        analysis = ips.analysis.PredictionMetrics(x=kernel_selection.atoms, y=prediction.atoms)
 
     proj.run()
 
@@ -61,4 +91,12 @@ def test_n_train_2_model(tmp_path, get_md22_stachyose):
     atoms.calc = model.get_calculator()
 
     assert atoms.get_potential_energy() < 0
-    assert atoms.calc.results["energy_uncertainty"] > 0
+
+    uncertainty_selection.load()
+    kernel_selection.load()
+    md.load()
+
+    uncertainties = [x.calc.results["energy_uncertainty"] for x in md.atoms]
+    assert [md.atoms[np.argmax(uncertainties)]] == uncertainty_selection.atoms
+
+    assert len(kernel_selection.atoms) == selection_batch_size
