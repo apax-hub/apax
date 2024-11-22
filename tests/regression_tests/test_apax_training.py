e0ffcc23ae2824e930aeebac67d3df334ca80b30
Tetracarbonylnickel <segreto@theochem.uni-stuttgart.de>
2024-11-22 13:27:33 +0000

    test update

--- tests/regression_tests/test_apax_training.py
+++ tests/regression_tests/test_apax_training.py
@@ -39,10 +39,10 @@ def test_regression_model_training(get_md22_stachyose, get_tmp_path):
     current_metrics = load_csv(working_dir / "test/log.csv")
 
     comparison_metrics = {
-        "val_energy_mae": 0.07457,
-        "val_forces_mae": 0.04777,
-        "val_forces_mse": 0.00423,
-        "val_loss": 0.05094,
+        "val_energy_mae": 0.075,
+        "val_forces_mae": 0.045,
+        "val_forces_mse": 0.004,
+        "val_loss": 0.045,
     }
     
     for key in comparison_metrics.keys():
@@ -50,5 +50,5 @@ def test_regression_model_training(get_md22_stachyose, get_tmp_path):
             
     for key in comparison_metrics.keys():
         assert (
-            abs(np.array(current_metrics[key])[-1] - comparison_metrics[key]) < 0.015
+            abs((np.array(current_metrics[key])[-1] - comparison_metrics[key])) < 0.01
         )
