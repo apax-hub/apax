73891b3da9cd3cf5a39bc52c82bfd90daa6a02ae
Tetracarbonylnickel <segreto@theochem.uni-stuttgart.de>
2025-05-06 11:16:33 +0200

    added FixLayerConstrain

--- apax/md/constraints.py
+++ apax/md/constraints.py
@@ -12,7 +12,7 @@ class ConstraintBase(BaseModel):
     the constraint during simulations.
     """
 
-    def create(self, ref_state) -> Callable:
+    def create(self, ref_stat, system) -> Callable:
         pass
 
 
@@ -22,7 +22,7 @@ class FixAtoms(ConstraintBase, extra="forbid"):
     name: Literal["fixatoms"] = "fixatoms"
     indices: list[int]
 
-    def create(self, ref_state) -> Callable:
+    def create(self, ref_state, system) -> Callable:
         indices = jnp.array(self.indices, dtype=jnp.int64)
 
         ref_position = ref_state.position[indices]
@@ -39,4 +39,33 @@ class FixAtoms(ConstraintBase, extra="forbid"):
         return fn
 
 
-Constraint = TypeAdapter(Union[FixAtoms]).validate_python
+class FixLayer(ConstraintBase, extra="forbid"):
+    """ """
+
+    name: Literal["fixlayer"] = "fixlayer"
+    upper_limit: float
+    lower_limit: float
+
+    def create(self, ref_state, system) -> Callable:
+        pos = ref_state.position @ system.box
+        z_coordinates = pos[:, 2]
+        
+        indices = jnp.where(
+            (self.lower_limit <= z_coordinates) & (z_coordinates <= self.upper_limit)
+        )
+
+        ref_position = ref_state.position[indices]
+
+        def fn(state):
+            position = state.position
+            force = state.force
+            zero_force = jnp.zeros_like(ref_position)
+            position = position.at[indices].set(ref_position)
+            force = force.at[indices].set(zero_force)
+            state = state.set(position=position, force=force)
+            return state
+
+        return fn
+    
+
+Constraint = TypeAdapter(Union[FixAtoms, FixLayer]).validate_python
