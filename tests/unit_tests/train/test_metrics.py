ca56442729a163cf31ffe01af7441b54aff5930d
Tetracarbonylnickel <segreto@theochem.uni-stuttgart.de>
2025-09-11 14:01:56 +0200

    test fix

--- tests/unit_tests/train/test_metrics.py
+++ tests/unit_tests/train/test_metrics.py
@@ -27,7 +27,7 @@ def test_cosine_sim():
         )
     }
 
-    angle_error = cosine_sim(label, prediction, "forces")
+    angle_error = cosine_sim(inputs= {}, label=label, prediction=prediction, key="forces")
     assert angle_error.shape == ()
     ref = 0.5
     assert abs(angle_error - ref) < 1e-6
@@ -53,21 +53,21 @@ def test_initialize_metrics_collection():
             ]
         ),
     }
-    input = {
+    inputs = {
         "n_atoms": jnp.array([2, 2]),
     }
     metrics_list = [
-        MetricsConfig(name="energy", reductions=["mae", "rmse", "per_atom_mae", "per_atoms_mse"]),
+        MetricsConfig(name="energy", reductions=["mae", "rmse", "per_atom_mae", "per_atom_mse"]),
         MetricsConfig(name="forces", reductions=["mae", "mse"]),
     ]
     Metrics = initialize_metrics(metrics_list)
-    batch_metrics = Metrics.single_from_model_output(input=input, label=label, prediction=prediction)
+    batch_metrics = Metrics.single_from_model_output(inputs=inputs, label=label, prediction=prediction)
 
     epoch_metrics = batch_metrics.compute()
 
     assert abs(epoch_metrics["energy_mae"] - 0.5) < 1e-6
     assert abs(epoch_metrics["energy_per_atom_mae"] - 0.25) < 1e-6
-    assert abs(epoch_metrics["energy_per_atom_mse"] - 0.125) < 1e-6
+    assert abs(epoch_metrics["energy_per_atom_mse"] - 0.25) < 1e-6
     assert abs(epoch_metrics["energy_rmse"] - jnp.sqrt(0.5)) < 1e-6
     assert abs(epoch_metrics["forces_mae"] - 1 / 3) < 1e-6
     assert abs(epoch_metrics["forces_mse"] - 1 / 3) < 1e-6
